81ff5a13db271594adc058729fd450af781a4120
Josh Barthelmess <joshbarthelmess3@gmail.com>
2021-04-29 19:47:54 -0400

    added some basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetOwnerComponent } from 'src/app/get-owner/get-owner.component'
+import { ListGuardService } from './list-guard.service';
+import { ListsComponent } from './lists/lists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GetOwnerComponent},
+  {
+    path: 'lists',
+    component: ListsComponent,
+    canActivate: [ListGuardService]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetOwnerComponent } from './get-owner/get-owner.component';
+import { ListsComponent } from './lists/lists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetOwnerComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
